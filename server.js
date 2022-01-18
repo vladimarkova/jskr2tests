ca404334a42aa01d4a30cd692a5bf3d86af32c84
Vladi Markova <vladislavam1996@gmail.com>
2022-01-18 11:05:23 +0200

    some middleware and custom middleware

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
+app.use(express.static('public')); // Suppose we have a public directory
+app.use(express.json()); // middleware to parse application/json content-type requests
+// the content will be added to the req.body object and we can easily use it
+
+const authorize = (req, res, next) => {
+    if (req.body.name !== 'Vladi') {
+        return res.status(401).send('Unauthorized!');
+    }
+    next();
+};
+
 app.get('/', (req, res) => {
     console.log(req.url);
     res.send('Hello from server! :)');
+})
+
+app.use('/', authorize, (req, res) => {
+    res.send('You connected with OK! :)');
 })
 
 app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}...`));
